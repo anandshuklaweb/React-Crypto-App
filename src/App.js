ea7826b76b93d99dfce5ce7211ed5f2a16242c19
Anand Shukla <anandshukla.web@gmail.com>
2022-06-10 17:15:05 +0530

    React Crypto App

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Crypto App <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <a className="designed" href="https://anandshukla.com">
+              Designed by Anand Shukla
+            </a>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
